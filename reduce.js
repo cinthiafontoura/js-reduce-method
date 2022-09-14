550589d16d64e2be64f896791f62b2fc961ee2e2
Cinthia Fontoura <cinthiafontouras@gmail.com>
2022-09-14 10:01:54 +0000

    Add reduce() examples

--- reduce.js
+++ reduce.js
@@ -6,6 +6,38 @@
 
 
 // Summing an array of numbers:
+const nums = [0, 1, 2, 3, 4];
+let sum = nums.reduce((acc, curr) => acc + curr); // acc = accumulator and curr = currentValue
+console.log (sum);
+
+// simplified to real life use
+const nums4 = [0, 1, 2, 3, 4];
+let sum4 = nums4.reduce((acc, curr) => acc + curr, 7); // the second parameter goes afer coma
+console.log (sum4);
+
+// expanded exmple 1
+const nums2 = [0, 1, 2, 3, 4];
+let sum2 = nums2.reduce((acc, curr) => {
+  console.log(
+    'Accumulator:', acc,
+    'Current Value:', curr,
+    'Total:', acc + curr
+  );
+  return acc + curr;
+});
+console.log (sum2);
+
+// expanded exmple 2
+const nums3 = [0, 1, 2, 3, 4];
+let sum3 = nums3.reduce((acc, curr) => {
+  console.log(
+    'Accumulator:', acc,
+    'Current Value:', curr,
+    'Total:', acc + curr
+  );
+  return acc + curr;
+}, 6); // This second paramter is the inial value to start with, if chanched to 10 will start countinf from 10 and adding the values of the array
+console.log (sum3);
 
 
 const teamMembers = [
